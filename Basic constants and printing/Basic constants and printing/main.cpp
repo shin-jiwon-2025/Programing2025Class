96c1d7b2dc28cc5d90dc0ca957ae26e03d7ae9e9
sungjin879 <4sungjin9@gmail.com>
2025-09-15 15:59:18 +0900

    summarry
    description

---
+++ Basic constants and printing/Basic constants and printing/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Name\t:\tSungwook\n");
+	printf("Initial\t:\tS\n");
+	printf("Age\t:\t%d\n", 28);
+	printf("score\t:\t%lf\n", 93.5);
+	printf("count\t:\t%u\n", 3000000000);
+
+		return 0;
+}
