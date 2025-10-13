a21c5c84453cd259752d54a39d22a785601f02a4
Egunbang <ericshin0598@gmail.com>
2025-10-13 15:25:14 +0900

    s
    d

--- Project6/Project6/main.cpp
+++ Project6/Project6/main.cpp
@@ -5,17 +5,22 @@
 int main(void)
 {
 	char chName[50] = " ";
-	int nstudentID = 99.0;
-	float fHeight = 999.0f;
-	float fWeight = 999.0f;
-	double dBMI = 999.0;
+	int nstudentID = 0;
+	float fHeight = 0.0f;
+	float fWeight = 0.0f;
+	double dBMI = 0.0;
 
-	printf("이름, 학번, 키(m),몸무게(kg)를 입력하세요: \n");
-	int nResscanf = scanf("%s%d%f%f", Name, &StudentID, &Height, &Weight);
-	dBMI = fHeight*fWeight;
-	printf("\")
+	printf("Input your information (Name, StudentID, Height(m), Weight(kg)): \n");
+	int nResScanf = scanf("%s%d%f%f", chName, &nstudentID, &fHeight, &fWeight);
+	dBMI = fWeight / (fHeight*fHeight);
+	printf("Output nResScanf: %d\n", nResScanf);
 
-
+	printf("\n=== Result===\n");
+	printf("Name: %s\n", chName);
+	printf("studentID: %d\n", nstudentID);
+	printf("Height: % .2f m\n", fHeight);
+	printf("Weight: % .2f kg\n", fWeight);
+	printf("Bmi: % .2f\n", dBMI);
 	
 	return 0;
 }
