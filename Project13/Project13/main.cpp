5866f5ae419605d24a824b45e26bab3d54282804
Egunbang <ericshin0598@gmail.com>
2025-10-22 09:31:45 +0900

    s
    d

--- Project13/Project13/main.cpp
+++ Project13/Project13/main.cpp
@@ -1,14 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warnings(disable: 4996)
 #include <stdio.h>
 
 int main(void)
 {
-	for (unsigned int i = 2; i <= 9; i++)
+	int dan;
+
+	printf("출력할 단을 입력하세요 (0 입력 시 전체 출력): ");
+	scanf("%d", &dan);
+
+	if (dan == 0)
 	{
-		for (unsigned int j = 1; j <= 9; j++)
+		for (int i = 2; i <= 9; i++)
 		{
-			printf("%d * %d = %d\n", i, j, i * j);
+			printf("\n[%d단]\n", i);
+			for (int j = 1; j <= 9; j++)
+			{
+				printf("%d * %d = %d\n", i, j, i * j);
+			}
 		}
-		printf("\n");
 	}
-	return 0;
+	else if (dan >= 2 && dan <= 9)
+	{
+		printf("\n[%d단]\n", dan);
+		for (int j = 1; j <= 9; j++)
+		{
+			printf("%d * %d = %d\n", dan, j, dan * j);
+		}
+	}
+	else
+	{
+		printf("잘못된 입력입니다. 2~9 또는 0을 입력하세요. \n");
+	}
+		return 0;
 }
