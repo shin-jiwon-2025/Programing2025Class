206f5107e676d0e8c365ba177619deabf420a848
Egunbang <ericshin0598@gmail.com>
2025-10-27 17:51:42 +0900

    d
    f

---
+++ Project16/Project16/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define NUM_PRINT 0
+void PrintHelloWorld(int nCount);
+int main(void)
+{
+	int nCount = 5;
+	PrintHelloWorld(nCount);
+	return 0;
+
+}
+
+void PrintHelloWorld(int nCount)
+{
+	if (nCount == NUM_PRINT)
+	{
+		return;
+	}
+	printf("Hello, World!(%d)\n", nCount);
+	PrintHelloWorld(++nCount);
+	printf("jam\n");
+	return;
+}
